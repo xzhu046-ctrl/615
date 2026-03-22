d2ea3f61c645c083cfcbaab8e4796c6a93d97b2f
Sherry Zhu <sherryzhu@Sherrys-MacBook-Air.local>
2026-03-22 02:27:58 -0600

    Add map app icon customization

--- main.js
+++ main.js
@@ -15,6 +15,7 @@ const HOME_ICON_DEFAULTS = {
   settings: '设置',
   customize: '外观',
   worldbook: '世界书',
+  map6: '地图',
 };
 const PHONE_FRAME_STORAGE_KEY = 'phone_frame_visible';
 const LIVE_DANMAKU_DEFAULTS = {
@@ -30,7 +31,7 @@ const MOMENTS_POSTS_KEY = 'qq_moments_posts';
 const MOMENTS_POSTS_ALT_KEY = 'moments_posts';
 const MOMENTS_LAST_SEEN_KEY = 'qq_moments_last_seen';
 const OFFLINE_MINIMIZED_CHAR_KEY = 'offline_minimized_char';
-const APP_BUILD_ID = '2026-03-22T08:23:38Z';
+const APP_BUILD_ID = '2026-03-22T08:27:36Z';
 const REFRESH_RECALC_FLAG_KEY = 'refresh_recalc_needed_v1';
 const UPDATE_PROMPT_DEDUPE_KEY = 'hosted_update_prompt_dedupe_v1';
 const UPDATE_PROMPT_DEDUPE_MS = 8000;
